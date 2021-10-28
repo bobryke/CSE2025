6b02a46d9bc9d1fb8928c4b719738660e1180e4c
Berkay Mengunogul <berkay.mengunogul@outlook.com>
2021-10-28 17:29:30 +0300

    Reading input file function improved and comments added

--- project_1/project_1.c
+++ project_1/project_1.c
@@ -2,105 +2,185 @@
 #include <string.h>
 #include <stdlib.h>
 
-
+// Node structure for linked list
 typedef struct node{
     int value;
     struct node* next; 
 } node;
 
+// Function for inserting a node to a given linked list
 void insertNode(node **head, int value){
     struct node* newNode = (node*) malloc(sizeof(node));
     newNode->value  = value;
-
-    if(*head == NULL){ // if starting pointer doesnt indicate any adress first node will be directed to the starting pointer
+    // if starting pointer doesnt indicate any adress first node will be directed to the starting pointer
+    if(*head == NULL){ 
             *head = newNode;
             return;
         }
 
+    // It skips over till last node where next node is empty
     node *currentPtr = *head;
     while (currentPtr->next != NULL){
             currentPtr = currentPtr->next;
         }
+    // links the new node to the end of linked list   
     currentPtr -> next = newNode;
 }
 
+// Function for multiplying the linked list and saving it into a another linked list
 void multiplyLists(node **multiplicand, node **multiplier, node **result){
+    
     // multiplier digits
     node *currentMultiplierPtr = *multiplier;
-    int multiplierIndex = 0;
-    int skipperFlag = 0;
+    int multiplierIndex = 0; // to track the index of multiplier di
+    int carry = 0; // to hold the carry digit of multiplication for next iteration
     while (currentMultiplierPtr != NULL){
             // multiplicand digits
             node *currentMultiplicandPtr = *multiplicand;
-            int carry = 0;
             node* currentResultPtr = *result;
+            
+            // it skips the multiplier digits by the index of multipler
+            // The reason is every currentMultiplierPtr digit is multiples of 10**index.
+            
+            // to illustrate; 
+            //   12
+            //   23
+            //x____
+            //  36 
+            // 24  --> 6 is skipped
+            //_____
+            // 276
+
+            for(int i = 0; i <multiplierIndex; i++){
+                    currentResultPtr = currentResultPtr->next;
+                }
+            
+            //goes through multiplicand digits till the last digit/
             while (currentMultiplicandPtr != NULL){
-                if(multiplierIndex == 0){
+                // if linked list of result indicates empty node then it inserts
+                if (currentResultPtr == NULL){
                     int value = currentMultiplierPtr->value * currentMultiplicandPtr->value + carry;
                     carry = (value / 10)%10; // update the carry in here
                     insertNode(result, value%10);
-                } else if(skipperFlag == 1){
-                    for(int i = 0; i <multiplierIndex; i++){
+                    currentResultPtr = *result;
+                    while (currentResultPtr != NULL){
                         currentResultPtr = currentResultPtr->next;
                     }
-                    int value = currentMultiplierPtr->value * currentMultiplicandPtr->value + carry + currentResultPtr->value;
-                    carry = (value / 10)%10; // update the carry in here
-                    currentResultPtr->value = value%10;
-                    skipperFlag = 0;
-                    currentResultPtr = currentResultPtr->next;
-                }else {
+                // else it updates the value of current node    
+                } else {
                     int value = currentMultiplierPtr->value * currentMultiplicandPtr->value + carry + currentResultPtr->value;
                     carry = (value / 10)%10; // update the carry in here
                     currentResultPtr->value = value%10;
                     currentResultPtr = currentResultPtr->next;
-                }
-                // handling if there is a carry at the end of multiplication
-                if ((currentMultiplicandPtr->next == NULL) && (carry != 0)) { 
-                    insertNode(result, carry);
-                    skipperFlag = 1;
                 }
                 currentMultiplicandPtr = currentMultiplicandPtr->next;
             }
-            // next multiplier digit
+            // at the end of the multiplication if there is any carry left, it will be inserted to linked list
+            if(carry != 0){
+            insertNode(result, carry);
+            carry = 0;
+        }
+            // update the pointer to next multiplier digit
             currentMultiplierPtr = currentMultiplierPtr->next;
+            // update the value of multiplier digit
             multiplierIndex += 1;
         }
+        // at the end of the multiplication if there is any carry left, it will be inserted to linked list
+        if(carry != 0){
+            insertNode(result, carry);
+            carry = 0;
+        }
+                   
     return;
 }
 
-void printNodes(node **head){
-    node *currentPtr = *head;
-    while (currentPtr != NULL){
-            printf("%d", currentPtr->value);
-            currentPtr = currentPtr->next;
-            
-        }
+// Fuction for reversing a linked. It is need beacuse while multiplying the numbers 
+// process happening from right to left
+// when it comes to printing the result it happens from left to right.
+// Since one-way linked lists used in this project it is a necessary function 
+void reverseList(node** head){
+    node* prevPtr = NULL;
+    node* currentPtr = *head;
+    node* nextPtr = NULL;
+    while(currentPtr != NULL){
+        nextPtr = currentPtr->next;
+        currentPtr->next = prevPtr;
+        prevPtr = currentPtr;
+        currentPtr = nextPtr;
+    }
+    *head = prevPtr;
 }
 
-int main(void){
+// write the results to the ouput file
+void printListsToFile(node **multiplicand, node **multiplier, node **result){
+    FILE *outputFilePtr;
+
+    // With using reverseList function, before printing the results I reversed the linked lists
+    // one by one
+    // The reason is digits are multiplied right from left but printed the opposite way.
+    reverseList(multiplicand);
+    reverseList(multiplier);
+    reverseList(result);
+
+    if ((outputFilePtr = fopen("output.txt", "w")) !=NULL) {
+        //write multiplicand
+        node *currentPtr = *multiplicand;
+        while (currentPtr != NULL){
+               fprintf(outputFilePtr, "%d", currentPtr->value);
+                currentPtr = currentPtr->next;
+                
+            }
+        fprintf(outputFilePtr, "\n");
+        //write multiplier
+        currentPtr = *multiplier;
+        while (currentPtr != NULL){
+               fprintf(outputFilePtr, "%d", currentPtr->value);
+                currentPtr = currentPtr->next;
+                
+            }
+        fprintf(outputFilePtr, "\n");
+        //write result
+        currentPtr = *result;
+        while (currentPtr != NULL){
+               fprintf(outputFilePtr, "%d", currentPtr->value);
+                currentPtr = currentPtr->next;
+                
+            }
+    }else{
+        printf("File could not saved");
+    }
+}
+
+int main(int argc, char* argv[]){
+    FILE *inputFilePtr;
     node* multiplicandHead = NULL;
     node* multiplierHead = NULL;
     node* resultHead = NULL;
 
-    char input_multiplicand[999] = "5301858469230152002541253";
-    char input_multiplier[999] = "8";
-    // read the input
-    //fgets(input_multiplicand, 999, stdin);
-    //fgets(input_multiplier, 999, stdin);
-    // delete the trailing '/n'
-    input_multiplicand[strcspn(input_multiplicand, "\n")] = 0;
-    input_multiplier[strcspn(input_multiplier, "\n")] = 0;
-    
-    for(int i = strlen(input_multiplicand)-1; i>=0; i--){
-        // with -48, the char is turned into the corresponding integer
-        insertNode(&multiplicandHead, input_multiplicand[i]-48);
-    }
+    // main function gets the dir of input file from command line
+    // main function gets the dir of input file from command line
+    if ((inputFilePtr = fopen(argv[1], "r")) != NULL) {
+        char input_multiplicand, input_multiplier;
 
-    for(int i = strlen(input_multiplier)-1; i>=0; i--){
-        // with -48, the char is turned into the corresponding integer
-        insertNode(&multiplierHead, input_multiplier[i]-48);
+        // creates a linked list for multiplicand from input
+        while (((input_multiplicand = fgetc(inputFilePtr)) != EOF) && (input_multiplicand != '\n'))
+        {   
+            insertNode(&multiplicandHead, atoi(&input_multiplicand));
+        }
+        
+        // creates a linked list for multiplier from input
+        while (((input_multiplier = fgetc(inputFilePtr)) != EOF) && (input_multiplier != '\n'))
+        {   
+            insertNode(&multiplierHead, atoi(&input_multiplier));
+        }
+
+        //reversing the linked lists beacuse numbers are multiplied right to left
+        reverseList(&multiplicandHead);
+        reverseList(&multiplierHead);
+
+        // multiples
+        multiplyLists(&multiplicandHead, &multiplierHead, &resultHead);
+        // puts the result to the output file
+        printListsToFile(&multiplicandHead, &multiplierHead, &resultHead);
     }
-    multiplyLists(&multiplicandHead, &multiplierHead, &resultHead);
-    printf("\nprinting nodes: ");
-    printNodes(&resultHead);
 }
