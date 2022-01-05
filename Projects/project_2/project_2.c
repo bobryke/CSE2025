38af6d1dfeab46b1f8a00184794543ebc2bb656d
Berkay Mengunogul <berkay.mengunogul@outlook.com>
2022-01-05 20:35:06 +0300

    Coding Implemetation of HW is finsihed.

--- Projects/project_2/project_2.c
+++ Projects/project_2/project_2.c
@@ -18,7 +18,7 @@ typedef struct Node{
 } node;
 
 
-// for sorting the nodes by their values by descending order
+// for sorting the nodes by their values in descending order
 void insertionSort(node *array, int numOfElements){
     node key;
     for(int i=0; i < numOfElements; i++){
@@ -32,54 +32,56 @@ void insertionSort(node *array, int numOfElements){
     }
 }
 
-// get height
-int getHeight(struct Node *N) {
-  if (N == NULL)
+// get height, it is necessary to handle empty childs giving errors
+int getHeight(node *curNode) {
+  if (curNode == NULL)
     return 0;
-  return N->height;
+  return curNode->height;
 }
 
-// right rotate
-node *rightRotate(node *y) {
-  node *x = y->leftChild;
-  node *T2 = x->rightChild;
+// right rotate (L/L Case)
+node *rightRotate(node *k2) {
+  node *k1 = k2->leftChild;
+  node *y = k1->rightChild;
 
-  x->rightChild = y;
-  y->leftChild = T2;
+  k1->rightChild = k2;
+  k2->leftChild = y;
 
-  y->height = MAX(getHeight(y->leftChild), getHeight(y->rightChild)) + 1;
-  x->height = MAX(getHeight(x->leftChild), getHeight(x->rightChild)) + 1;
+  // update heights
+  k2->height = MAX(getHeight(k2->leftChild), getHeight(k2->rightChild)) + 1;
+  k1->height = MAX(getHeight(k1->leftChild), getHeight(k1->rightChild)) + 1;
 
-  return x;
+  return k1;
 }
 
-// left rotate
-node *leftRotate(node *x) {
-  node *y = x->rightChild;
-  node *T2 = y->leftChild;
+// left rotate (R/R case)
+node *leftRotate(node *k1) {
+  node *k2 = k1->rightChild;
+  node *y = k2->leftChild;
 
-  y->leftChild = x;
-  x->rightChild = T2;
+  k2->leftChild = k1;
+  k1->rightChild = y;
 
-  x->height = MAX(getHeight(x->leftChild), getHeight(x->rightChild))+ 1;
-  y->height = MAX(getHeight(y->leftChild), getHeight(y->rightChild))+ 1;
+  k1->height = MAX(getHeight(k1->leftChild), getHeight(k1->rightChild))+ 1;
+  k2->height = MAX(getHeight(k2->leftChild), getHeight(k2->rightChild))+ 1;
 
   return y;
 }
 
-// Get the balance factor
-int checkBalance(node *N) {
-  if (N == NULL)
+// check balance of nodes
+int checkBalance(node *curNode) {
+  if (curNode == NULL)
     return 0;
-  return getHeight(N->leftChild) - getHeight(N->rightChild);
+  return getHeight(curNode->leftChild) - getHeight(curNode->rightChild);
 }
 
-// Insert node
+// Insert node like an AVL tree, nodes are inserted in AVL manner till compelete BST is achieved.
 node *insertAVL(node *curNode, node *keyNode) {
   // Find the correct position to insertNode the node and insertNode it
   if (curNode == NULL)
     return keyNode;
 
+  // find the right place to insert the node
   if (keyNode->value < curNode->value)
     curNode->leftChild = insertAVL(curNode->leftChild, keyNode);
   else if (keyNode->value > curNode->value)
@@ -87,67 +89,79 @@ node *insertAVL(node *curNode, node *keyNode) {
   else
     return curNode;
 
-  // Update the balance factor of each node and
-  // Balance the tree
-  curNode->height = 1 + MAX(getHeight(curNode->leftChild),
-               getHeight(curNode->rightChild));
+  // Update the height of the nodes
+  curNode->height = MAX(getHeight(curNode->leftChild), getHeight(curNode->rightChild))+1;
 
-  int balance = checkBalance(curNode);
-  if (balance > 1 && keyNode->value < curNode->leftChild->value)
+  // apply rotations to balance the tree
+  int balanceCondition = checkBalance(curNode);
+  // L/L case
+  if (keyNode->value < curNode->leftChild->value && balanceCondition > 1)
     return rightRotate(curNode);
-
-  if (balance < -1 && keyNode->value > curNode->rightChild->value)
+  // R/R case
+  if (keyNode->value > curNode->rightChild->value && balanceCondition < -1)
     return leftRotate(curNode);
-
-  if (balance > 1 && keyNode->value > curNode->leftChild->value) {
+  // R/L case
+  if (keyNode->value > curNode->leftChild->value && balanceCondition > 1) {
     curNode->leftChild = leftRotate(curNode->leftChild);
     return rightRotate(curNode);
   }
-
-  if (balance < -1 && keyNode->value < curNode->rightChild->value) {
+  // L/R case
+  if (keyNode->value < curNode->rightChild->value && balanceCondition < -1) {
     curNode->rightChild = rightRotate(curNode->rightChild);
     return leftRotate(curNode);
   }
 
+  // return new root node
   return curNode;
 }
 
- node *insertBST(node *curNode, node *newNode){
-   if (curNode == NULL){
-     return newNode;
-   }
-   if(newNode->value > curNode->value){
-     curNode->rightChild = insertBST(curNode->rightChild, newNode);
-   }else if(newNode->value < curNode->value){
-     curNode->leftChild = insertBST(curNode->leftChild, newNode);
-   }
-   return curNode;
- }
-
-node *minValuenode(node *N) {
-  node *current = N;
-
-  while (current->leftChild != NULL)
-    current = current->leftChild;
-
-  return current;
+// Insert node like an BST tree, remaining nodes after complete BST are inserted in BST manner.
+node *insertBST(node *curNode, node *newNode){
+  if (curNode == NULL){
+    return newNode;
+  }
+  if(newNode->value > curNode->value){
+    curNode->rightChild = insertBST(curNode->rightChild, newNode);
+  }else if(newNode->value < curNode->value){
+    curNode->leftChild = insertBST(curNode->leftChild, newNode);
+  }
+  return curNode;
 }
 
-void printOutput(int numberOfFullDepthLevels, int numberOfDepthLevels, int numberOfAccesiveNodes, int remainingNodes){
+
+void printOutput(int numberOfFullDepthLevels, int numberOfDepthLevels, int numberOfAccesiveNodes, int remainingNodes, int getCase){
   printf("Output:\n");
   printf("Depth level of BST is %d\n", numberOfDepthLevels);
-  int depthLevel;
-  for (depthLevel = 0; depthLevel< numberOfFullDepthLevels; depthLevel++){
-    printf("Depth level %d -> %d\n", depthLevel, (int)pow(2,depthLevel));
-  }
-  printf("Depth level %d -> %d\n", depthLevel++, numberOfAccesiveNodes);
-  while(remainingNodes>0){
-    printf("Depth level %d -> %d\n", depthLevel++, 1);
-    remainingNodes--;
-  }
-  
-}
+  // show nodes in depth levels for case 1
+  if (getCase == 1){
+    int depthLevel;
+    for (depthLevel = 0; depthLevel< numberOfFullDepthLevels; depthLevel++){
+      printf("Depth level %d -> %d\n", depthLevel, (int)pow(2,depthLevel));
+    }
+    printf("Depth level %d -> %d\n", depthLevel++, numberOfAccesiveNodes);
+    while(remainingNodes>0){
+      printf("Depth level %d -> %d\n", depthLevel++, 1);
+      remainingNodes--;
+    }
+  // show depth levels for case 2. (Borrow nodes from Complete BST of the tree.)
+  }else if(getCase == 2){
+    int depthLevel;
+    
+    for (depthLevel = 0; depthLevel< numberOfFullDepthLevels-1; depthLevel++){
+      printf("Depth level %d -> %d\n", depthLevel, (int)pow(2,depthLevel));
+    }
 
+    // took one node from last full node
+    // and gave it to accessive depth level (one below of complete binary tree)
+    printf("Depth level %d -> %d\n", depthLevel, (int)pow(2,depthLevel)-1);
+    printf("Depth level %d -> %d\n", ++depthLevel, numberOfAccesiveNodes=1);
+    while(remainingNodes>0){
+      printf("Depth level %d -> %d\n", ++depthLevel, 1);
+      remainingNodes--;
+    }
+  }
+}
+// find the key and update its information about depth and position.
 node *findKey(node *currentNode, int key, int position, int depth){
   if(currentNode != NULL){
     if(currentNode->value == key){
@@ -161,14 +175,6 @@ node *findKey(node *currentNode, int key, int position, int depth){
     }
   }
   return NULL;
-}
-
-int checkCase(int numOfNodes){
-  if (ceil(log2(numOfNodes)) == floor(log2(numOfNodes))){
-    return 2;
-  }
-  else
-    return 1;
 }
 
 int main(void){
@@ -185,29 +191,35 @@ int main(void){
       // creates a linked list for multiplicand from input
       while (fscanf(inputFilePtr, " %10s", currentNumber) == 1)
       {   
+
           // check node, if less than zero
+          int zeroFL = 0;
+          int replicatedFL = 0;
           if(atoi(currentNumber) <= 0){
-              printf("Input file contains values less than or equal to zero.\n");
-              return -1;
+              printf("[ERROR]: Input file contains values less than or equal to zero. %d not inserted to BST.\n", atoi(currentNumber));
+              zeroFL = 1;
           }
           // check node, if replicate node occurs
           for(int i=0; i < countnodes; i++){
               if (arrayOfnodes[i].value == atoi(currentNumber)){
-                  printf("Input file contains replicated keys.\n");
-                  return -1;
+                  printf("[ERROR]: Input file contains replicated keys. %d not inserted to BST.\n", atoi(currentNumber));
+                  replicatedFL = 1;
               }
           }
-
-          node newnode;
-          newnode.value = atoi(currentNumber);
-          newnode.height = 1;
-          newnode.position = 1;
-          newnode.depthLevel = 0;
-          newnode.leftChild = NULL;
-          newnode.rightChild = NULL;
-          arrayOfnodes[countnodes] = newnode; 
-          countnodes++;
-          arrayOfnodes = realloc(arrayOfnodes, countnodes+1 * sizeof(node));
+          // if no negative value and no replicated key then create the node.
+          if (zeroFL == 0 && replicatedFL == 0){
+            node newnode;
+            newnode.value = atoi(currentNumber);
+            newnode.height = 1;
+            newnode.position = 1;
+            newnode.depthLevel = 0;
+            newnode.leftChild = NULL;
+            newnode.rightChild = NULL;
+            arrayOfnodes = realloc(arrayOfnodes, countnodes * (sizeof(node)+1));
+            arrayOfnodes[countnodes] = newnode; 
+            countnodes++;
+            
+          }
       }
         
       // check number of nodes, give error if less than 16.
@@ -219,43 +231,62 @@ int main(void){
       // sort the array in descending order.
       insertionSort(arrayOfnodes, countnodes);
 
-      int numberOfFullDepthLevels = (int)floor(log2(countnodes));
-      int numberOfDepthLevels = 3*floor(log(countnodes)/log(4));
-      int numberOfAccesiveNodes = countnodes - (int)pow(2, floor(log2(countnodes)))+1-numberOfDepthLevels+numberOfFullDepthLevels+1;
-      int remainingNodes = countnodes-(int)pow(2, numberOfFullDepthLevels)+1-numberOfAccesiveNodes;
-      printOutput(numberOfFullDepthLevels, numberOfDepthLevels, numberOfAccesiveNodes, remainingNodes);
+      //check if number of created nodes are power of 2
+      int checkCase = ((ceil(log2(countnodes)) == floor(log2(countnodes)))? 2 : 1);
       
-      if(checkCase(countnodes) == 1){
+      //get the depth level of BST part
+      int numberOfFullDepthLevels = (int)floor(log2(countnodes));
+      // get total depth levels
+      int numberOfDepthLevels = 3*floor(log(countnodes)/log(4));
+      // get the number nodes after BST. it is mentioned as Accesive Nodes in the lecture.
+      int numberOfAccesiveNodes = countnodes - (int)pow(2, floor(log2(countnodes)))+1-numberOfDepthLevels+numberOfFullDepthLevels+1;
+      // get the remaining nodes considering calculations above
+      int remainingNodes = countnodes-(int)pow(2, numberOfFullDepthLevels)+1-numberOfAccesiveNodes;
+      printOutput(numberOfFullDepthLevels, numberOfDepthLevels, numberOfAccesiveNodes, remainingNodes, checkCase);
 
+      //check if it is case 1 or case 2.
+      if(checkCase == 1){
+        //case 1
         int i, j;
         for (i = 0; i < countnodes-remainingNodes; i++){
             root = insertAVL(root, &arrayOfnodes[i]);
-            printf("node avl %d\n", arrayOfnodes[i].value);
         }
 
         for (j = 0; j < remainingNodes; j++)
         {
             insertBST(root, &arrayOfnodes[i+j]);
-            printf("node remaining %d\n", arrayOfnodes[i+j].value);
         }
 
-        int input;
-        while(input != 0){
-          printf("\nKey value to be searched (Enter 0 to exit) :");
-          scanf("%d", &input);
-          node *keyNode = findKey(root, input, 1, 0);
-          if(keyNode == NULL && input != 0){
-            printf("%d is not found in BST", input);
-          }else{
-            printf("At Depth level %d, %d. element", keyNode->depthLevel, keyNode->position);
-          } 
-        }
       } else {
         // case 2
+        int i, j;
+        // take one node from Compelete BST
+        for (i = 0; i < countnodes-remainingNodes-1; i++){
+            root = insertAVL(root, &arrayOfnodes[i]);
+        }
+        // give it to lower level
+        for (j = 0; j < remainingNodes+1; j++)
+        {
+            insertBST(root, &arrayOfnodes[i+j]);
+        }
+      }
+
+      //find key section
+      int input = 1;
+      while(input > 0){
+        printf("\nKey value to be searched (Enter 0 to exit) :");
+        scanf("%d", &input);
+        node *keyNode = findKey(root, input, 1, 0);
+        if(input <= 0){
+          printf("Exit\n");
+          return -1;
+        }else if(keyNode == NULL && input != 0){
+          printf("%d is not found in BST", input);
+        }else{
+          printf("At Depth level %d, %d. element", keyNode->depthLevel, keyNode->position);
+        } 
       }
     }
-
-    printf("Exit\n");
     free(arrayOfnodes);
     return 0;
 }
--- Projects/project_2/tempCodeRunnerFile.c
+++ Projects/project_2/tempCodeRunnerFile.c
@@ -1,5 +1,1 @@
-for (j = 0; j < remainingNodes; j++)
-      {
-          insertBST(root, &arrayOfnodes[i+j]);
-          printf("node remaining %d\n", arrayOfnodes[i+j].value);
-      }
+depthLevel
